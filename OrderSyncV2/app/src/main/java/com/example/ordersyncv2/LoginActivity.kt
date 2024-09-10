4a3bd2a76740b33bfb51601dc099a6be4bf6ad35
César Grazina Scifoni <cesarscifoni@gmail.com>
2024-09-10 18:11:15 -0300

    Inserindo o projeto no git

---
+++ OrderSyncV2/app/src/main/java/com/example/ordersyncv2/LoginActivity.kt
@@ -0,0 +1,4 @@
+package com.example.ordersync
+
+class LoginActivity {
+}
---
+++ OrderSyncV2/app/src/main/java/com/example/ordersyncv2/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.ordersync
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.view.View
+import com.example.ordersyncv2.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        //Barra de status transparente
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        window.statusBarColor = android.graphics.Color.TRANSPARENT
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
