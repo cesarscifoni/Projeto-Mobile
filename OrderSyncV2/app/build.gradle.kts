b439c0ee305e5d2a2184e7bd66a1e9e15caf842b
César Grazina Scifoni <cesarscifoni@gmail.com>
2024-09-11 17:16:58 -0300

    Atuallizações de Dependências e de Tela de Login

--- OrderSyncV2/app/build.gradle.kts
+++ OrderSyncV2/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
+
 }
 
 android {
@@ -37,11 +39,14 @@ android {
 
 dependencies {
 
+    implementation(platform("com.google.firebase:firebase-bom:33.2.0"))
+    implementation("com.google.firebase:firebase-analytics")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
